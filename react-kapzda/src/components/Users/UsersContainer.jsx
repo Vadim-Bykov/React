6efd68547e5de2dac07e397aa878fc5364b2d27b
Vadim <bvntaev@gmail.com>
2021-01-17 18:18:11 +0300

    feat: add preloader

--- react-kapzda/src/components/Users/UsersContainer.jsx
+++ react-kapzda/src/components/Users/UsersContainer.jsx
@@ -1,37 +1,51 @@
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/users-reducer";
+import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC, toggleIsFetchingAC } from "../../redux/users-reducer";
 import React from "react";
 import axios from 'axios';
 import Users from './Users';
+import Preloader from "../common/preloader/Preloader";
 
 class UsersAPIComponent extends React.Component  {
    
    componentDidMount = () => {
+      this.props.toggleIsFetching(true)
       axios
          .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
          .then(res => {
+            this.props.toggleIsFetching(false)
             this.props.setTotalUsersCount(res.data.totalCount);
             this.props.setUsers(res.data.items)
          })
-   }
+   };
 
    changePage=(pageNumber)=> {
+      this.props.toggleIsFetching(true)
       this.props.setCurrentPage(pageNumber);
       axios
          .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-         .then(res => this.props.setUsers(res.data.items));
+         .then(res => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(res.data.items)
+         });
    }
 
    render = () => {
-      return <Users
-         users={this.props.users}
-         totalUsersCount={this.props.totalUsersCount}
-         pageSize={this.props.pageSize}
-         currentPage={this.props.currentPage}
-         follow={this.props.follow}
-         unfollow={this.props.unfollow}
-         changePage={this.changePage}
-      />
+      return (
+         <>
+            {this.props.isFetching
+               ? <Preloader/>
+               : null}
+            <Users
+               users={this.props.users}
+               totalUsersCount={this.props.totalUsersCount}
+               pageSize={this.props.pageSize}
+               currentPage={this.props.currentPage}
+               follow={this.props.follow}
+               unfollow={this.props.unfollow}
+               changePage={this.changePage}
+            />
+         </>
+      )
    };
 };
 
@@ -41,6 +55,7 @@ const mapStateToProps = (state) => {
       pageSize: state.usersPage.pageSize,
       currentPage: state.usersPage.currentPage,
       totalUsersCount: state.usersPage.totalUsersCount,
+      isFetching: state.usersPage.isFetching,
    }
 }
 
@@ -56,7 +71,8 @@ const mapDispatchToProps = (dispatch) => {
          dispatch(setUsersAC(users));
       },
       setCurrentPage: (pageNumber)=> dispatch(setCurrentPageAC(pageNumber)),
-      setTotalUsersCount: (pageNumber)=> dispatch(setTotalUsersCountAC(pageNumber))
+      setTotalUsersCount: (pageNumber)=> dispatch(setTotalUsersCountAC(pageNumber)),
+      toggleIsFetching: (isFetching)=> dispatch(toggleIsFetchingAC(isFetching)),
 
    }
 }
---
+++ react-kapzda/src/components/common/preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import style from './Preloader.module.css';
+import preloader from '../../../assets/img/circles.svg';
+
+const Preloader = () => {
+  return (
+    <div className={style.preloaderBlock}>
+      <img className={style.preloader} src={preloader} fill='#000' />
+    </div>
+  );
+};
+
+export default Preloader;

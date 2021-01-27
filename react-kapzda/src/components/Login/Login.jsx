76e76c5c78b25eb2eca231f0d1972ef4be3b04a8
Vadim <bvntaev@gmail.com>
2021-01-27 23:31:50 +0300

    feat: add form

--- react-kapzda/src/components/Login/Login.jsx
+++ react-kapzda/src/components/Login/Login.jsx
@@ -1,5 +1,38 @@
+import { Field, Form, reduxForm } from "redux-form"
+
+const LoginForm = (props) => {
+   return (
+      <Form onSubmit={props.handleSubmit}>
+         <div>
+            <Field type="text" name="login" component="input" placeholder="login"/>
+         </div>
+         <div>
+            <Field type="text" name="password" component="input" placeholder="password"/>
+         </div>
+         <div>
+            <Field type="checkbox" component="input" name="rememberMe" /> Remember me
+         </div>
+         <div>
+            <button>Login</button>
+         </div>
+      </Form>
+   )
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
+
 const Login = () => {
-   return <h1>LOGIN</h1>
+   
+   const onSubmit = (formData) => {
+      console.log(formData)
+   }
+
+   return (
+      <div>
+         <h1>LOGIN</h1>
+         <LoginReduxForm onSubmit={onSubmit} />
+      </div>
+   )
 }
 
 export default Login;
